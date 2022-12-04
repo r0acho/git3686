65fef5b680396f9371bd3f91f579de8a7e0e5ff1
Photomann86 <kunzler555@gmail.com>
2022-12-04 22:24:53 +0500

    решена задача 6

---
+++ task6/task6.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, котррая на вход принимает целое число
+//и проверяет его на чётность
+Console.WriteLine("введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)  { Console.WriteLine("введённое число чётное"); } 
+    else Console.WriteLine("число нечётное");
