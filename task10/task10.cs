6c7bd233a90303e81faa83a6e3734fcc3f5a03d7
Photomann86 <kunzler555@gmail.com>
2022-12-05 21:54:11 +0500

    задача 10 готова

---
+++ task10/task10.cs
@@ -0,0 +1,5 @@
+﻿//Написать программу, которая принимает на вход трёхзначное число и выводит 2ю цифру этого числа
+Console.WriteLine("введите трёхзначное число");
+int num3 = Convert.ToInt16(Console.ReadLine());
+int dig2 = num3/10%10;
+Console.WriteLine(dig2);
