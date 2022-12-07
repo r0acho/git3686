cf9cf0be424c1b7094970227fd68269c2496f0ab
Photomann86 <kunzler555@gmail.com>
2022-12-07 19:47:12 +0500

    добавлено условие на ввод

--- task10/task10.cs
+++ task10/task10.cs
@@ -1,5 +1,8 @@
 ﻿//Написать программу, которая принимает на вход трёхзначное число и выводит 2ю цифру этого числа
 Console.WriteLine("введите трёхзначное число");
 int num3 = Convert.ToInt16(Console.ReadLine());
-int dig2 = num3/10%10;
-Console.WriteLine(dig2);
+if (num3<100 || num3 > 999) Console.WriteLine("введённое число вне требуемого диапазона");
+else {
+    int dig2 = num3/10%10;
+    Console.WriteLine($"{dig2}");
+    }
