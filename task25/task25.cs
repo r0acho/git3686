183841d532bc80ce940a29ee04928fe97f2c33ee
Photomann86 <kunzler555@gmail.com>
2022-12-11 21:37:01 +0500

    программа возведения в степень, вариант 1

---
+++ task25/task25.cs
@@ -0,0 +1,20 @@
+﻿/* Программа принимает от пользователя два числа и возводит одно в степень другого 
+Например А и В. Написать код без использование функции Math.Pow и ей подобных 
+5, 3 -> 125
+6, 4 -> 1296 */
+Console.WriteLine("введите основание степени");
+int osnova = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите показатель степени");
+int step = Convert.ToInt32(Console.ReadLine());
+double result = 1;
+if (step == 0) {Console.WriteLine($"{osnova}^0 = 1"); }
+    else if (step>0) 
+    {
+        for (int i = 1; i <= step; i++)
+        result *= osnova;
+    }
+    else if (step<0) 
+    {
+        for (int i = -1; i>= step; --i) result /= osnova;
+    }
+Console.WriteLine($"{result:f6}");
