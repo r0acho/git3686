2e9afea201b89f011399e3b64087611bceeedb69
Photomann86 <kunzler555@gmail.com>
2022-12-11 23:17:16 +0500

    задача решена с применением методов

---
+++ task27/task27.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая принимает число и считает сумму цифр в числе
+int ReadNum(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetSummDigits(int number)
+{
+    int sum = 0;
+    if (number < 0) 
+    { 
+        Console.WriteLine("ввели отрицательное число? Ща по модулю возмём да сумму посчитаем");
+        number = Math.Abs(number);
+        while (number > 0)
+        {
+          sum = sum + number %10;
+          number /= 10;  
+        }
+        return sum;
+    }
+    else 
+    {
+        while (number > 0)
+        {
+            sum = sum + number % 10;
+            number /= 10;
+        }
+    }
+    return sum;
+}
+int num = ReadNum("введите число");
+int result = GetSummDigits(num);
+Console.WriteLine($"сумма цифр {num} = {result}");
