da7d4ed925194138f4014edec067044e92e3453f
Photomann86 <kunzler555@gmail.com>
2022-12-13 20:05:57 +0500

    добавлен вариант решения с применением методов

--- task25/task25.cs
+++ task25/task25.cs
@@ -2,7 +2,7 @@
 Например А и В. Написать код без использование функции Math.Pow и ей подобных 
 5, 3 -> 125
 6, 4 -> 1296 */
-Console.WriteLine("введите основание степени");
+/*Console.WriteLine("введите основание степени");
 int osnova = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите показатель степени");
 int step = Convert.ToInt32(Console.ReadLine());
@@ -17,4 +17,32 @@ if (step == 0) {Console.WriteLine($"{osnova}^0 = 1"); }
     {
         for (int i = -1; i>= step; --i) result /= osnova;
     }
-Console.WriteLine($"{result:f6}");
+Console.WriteLine($"{result:f6}");*/
+int ReadIntOsn(string message1)
+{
+    Console.WriteLine(message1);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int ReadIntStep(string message2)
+{
+    Console.WriteLine(message2);
+    return Convert.ToInt32(Console.ReadLine());
+}
+double StepOfNumber(int number, int step)
+{
+    double res = 1;
+    if (step == 0) { Console.WriteLine("любое число в нулевой степени - 1"); }
+    else if (step>0) {
+        for (int i=1; i<=step; i++)
+        res *= number;
+    }
+    else if (step<0) {
+        for (int i = -1; i>=step; --i) res /= number;
+    }
+    return res;
+}
+int osnova = ReadIntOsn("введите основание степени");
+int steppow = ReadIntStep("введите показатель степени ");
+double result = StepOfNumber(osnova, steppow);
+Console.WriteLine($"{osnova}^{steppow} = {result:f4}");
+
