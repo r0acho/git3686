74c8d1ee4007c0676a57f30f2b11ab965ba060ba
Photomann86 <kunzler555@gmail.com>
2022-12-20 20:51:28 +0500

    task35 successfully solved with methods

---
+++ task35/task35.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 35: Задайте одномерный массив из 123 случайных чисел 
+в диапазоне [0, 150]. Найдите количество элементов массива, 
+значения которых лежат в отрезке [10,99]. */
+
+int[] SuperMassive(int size, int minRange, int maxRange)
+	{
+        int[] massiv = new int[size];
+		Random rand = new Random();
+		for (int i=0; i<massiv.Length; i++)
+		{ massiv[i]=rand.Next(minRange,maxRange+1); }
+	    return massiv;
+   }
+int CountNumbersRange(int[] massiv)
+    {
+	int count = 0;
+	for (int i=0; i<massiv.Length; i++)
+		{
+		if (massiv[i]>=10 && massiv[i]<=99)
+			count++;
+		}
+		return count;
+	}
+const int MINRANGE = 0;
+const int MAXRANGE = 150;
+const int SIZE = 123;
+int[] megArray = SuperMassive(SIZE, MINRANGE, MAXRANGE); 
+Console.WriteLine($"[{string.Join(", ", megArray)}]");
+int rangeCount = CountNumbersRange(megArray);
+Console.WriteLine($"Количество элементов в диапазоне [10;99]: {rangeCount}");
+
+
+
