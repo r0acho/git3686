51590e59aa847fb457f5ddedc1b4df4e4971c3cf
Photomann86 <kunzler555@gmail.com>
2022-12-22 21:33:51 +0500

    Задача 34 решена

---
+++ task36/task36.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+int[] GetRandomArray(int size, int leftRange, int rightRange)
+{
+    int[] massiv = new int[size];
+    Random rand = new Random();
+    for (int i=0; i<massiv.Length; i++)
+    {
+        massiv[i] = rand.Next(leftRange, rightRange+1);
+    }
+    return massiv;
+}
+int SummNonevenPos(int[] massiv)
+{
+    int summ = 0;
+    for (int index=1; index<massiv.Length; index+=2)
+    {
+        summ = summ+massiv[index];
+    }
+    return summ;
+}
+Console.WriteLine("введите размерность массива");
+int SIZE = Convert.ToInt32(Console.ReadLine());
+//const int SIZE = 10;
+const int LEFTR = -20;
+const int RIGHTR = 20;
+int[] Array = GetRandomArray(SIZE,LEFTR, RIGHTR);
+Console.WriteLine($"[{string.Join(", ", Array)}]");
+int Sum = SummNonevenPos(Array);
+Console.WriteLine($"Сумма элементов нечётных позиций = {Sum}");
