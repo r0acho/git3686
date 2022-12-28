b7deff55c3d77a6a63edabd4a30ab3318c98cf66
Photomann86 <kunzler555@gmail.com>
2022-12-28 22:04:38 +0500

    task67 resolved successfully

---
+++ task67/task67.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("vvedite chislo");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int Line(int sum)
+{
+  if(sum == 0) return 0;
+  else 
+  {
+    //int temp = 0;
+    sum = sum%10 + Line(sum/10);
+
+  }
+  return sum;
+   
+
+}
+Console.WriteLine(Line(num));
