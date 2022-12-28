89d19277aa1c41001695ddfa7eab4a709cc1b231
Photomann86 <kunzler555@gmail.com>
2022-12-28 22:02:40 +0500

    task69 resolved successfully

---
+++ task69/task69.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("vvedite основание и степень");
+int osn = Convert.ToInt32(Console.ReadLine());
+int step = Convert.ToInt32(Console.ReadLine());
+double Stepen(int n, int m)
+{
+    if(m == 0) return 1;
+    n *= Stepen(n, m-1);
+    return n;
+}
+Console.WriteLine(Stepen(osn, step));
