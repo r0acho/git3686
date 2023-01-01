9cc8ff0e0bac3db2ec4ddf2808f76363c0e822dc
Photomann86 <kunzler555@gmail.com>
2023-01-02 00:09:00 +0500

    task50 resolved successfully

---
+++ task47/task47.cs
@@ -0,0 +1,41 @@
+﻿/* задайте двумерный массив, заполненный случайными 
+вещественными числами
+m = 3, n = 4 - запрашиваются у пользователя
+-1.5 5.7 6.4 -1.4
+1.6 -3.4 11.7 7.1
+11.5 -1.1 6.4 -2.1
+*/
+int ReadNumbers(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+double[,] GetRandomArray(int rows, int columns, double leftR = -10.0, double rightR = 10.0)
+{
+    double[,] matr = new double[rows, columns];
+    var rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Convert.ToDouble(rand.Next(-100,100)/10.00);
+        }
+    }
+    return matr;
+}
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,6:F1}", matr[i, j]));
+        }
+        Console.WriteLine();
+    }
+}
+int rowsCount = ReadNumbers("количество строк: ");
+int columnCount = ReadNumbers("количество столбцов: ");
+double[,] matrix = GetRandomArray(rowsCount, columnCount);
+PrintArray(matrix);
+
---
+++ task50/task50.cs
@@ -0,0 +1,52 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+​Например, задан массив:
+​1 4 7 2
+​
+5 9 2 3
+​
+8 4 2 4
+​
+1 7 -> такого числа в массиве нет
+​*/
+int ReadNum(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[,] RandoMatrix(int leftR, int rightR, int rows, int cols)
+{
+    int[,] matrix = new int[rows,cols];
+    Random rand = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftR, rightR + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int LEFTR = ReadNum("нижний предел массива: ");
+int RIGHTR = ReadNum("верхний предел массива: ");
+int rowsCount = ReadNum("количество строк: ");
+int colsCount = ReadNum("количество столбцов: ");
+int[,] newMatrix = RandoMatrix(LEFTR, RIGHTR, rowsCount, colsCount);
+PrintMatrix(newMatrix);
+int ROWS = ReadNum("индекс строки");
+int COLS = ReadNum("индекс столбца");
+if (ROWS<newMatrix.GetLength(0) && COLS<newMatrix.GetLength(1))
+Console.WriteLine($"найденное число: {newMatrix[ROWS, COLS]}");
+else Console.WriteLine($"[{ROWS} {COLS}] числа с такими индексами нет в массиве");
