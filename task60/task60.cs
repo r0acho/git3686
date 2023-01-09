84ff4a0c74eb4afe36d528243acb1e10cdcf2086
Photomann86 <kunzler555@gmail.com>
2023-01-09 21:58:35 +0500

    task60 добавлена возможность вводить размерность трёхмерного массива

--- task60/task60.cs
+++ task60/task60.cs
@@ -2,8 +2,13 @@
 двузначных чисел. 
 Напишите программу, которая будет построчно выводить массив, 
 добавляя индексы каждого элемента */
-
-int[,,] Matrix3d = new int[2, 2, 2];
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int n = ReadInt("введите размерность массива: ");
+int[,,] Matrix3d = new int[n, n, n];
 FillArray(Matrix3d);
 PrintIndex(Matrix3d);
 
@@ -25,16 +30,16 @@ void PrintIndex(int[,,] arr)
 }
 
 // Функция заполнения 3D массива не повторяющимеся числами
-void FillArray(int[,,] arr)
+void FillArray(int[,,] array)
 {
     int count = 10;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int k = 0; k < arr.GetLength(2); k++)
+            for (int k = 0; k < array.GetLength(2); k++)
             {
-                arr[k, i, j] += count;
+                array[k, i, j] += count;
                 count += 3;
             }
         }
