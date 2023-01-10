bd00d8d9409243605afa8e3a11ffa834775524ad
Photomann86 <kunzler555@gmail.com>
2023-01-10 22:53:40 +0500

    task is resolved

---
+++ task66/task66.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 66:
+Задайте значения M и N. Напишите программу
+которая найдёт сумму натуральных чисел
+в диапазоне [M; N] */
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void NaturlichSumm(int M, int N, int summ)
+{
+    summ = summ+N;
+    if (N<=M)
+    {
+        Console.Write($"Сумма элементов: {summ}");
+        return;
+    }
+    NaturlichSumm(M, N-1, summ);
+}
+int M = ReadNumber("M = ");
+int N = ReadNumber("N = ");
+int step = M;
+NaturlichSumm(M,N,step=0);
